adc03451c9504bbee7b30f93de98ab0e68db696d
Josh Genereux <jgenereux@stratacache.com>
2024-01-10 16:04:47 -0500

    got gmail api working, and information passed to the front end.

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const { authenticate } = require('@google-cloud/local-auth');
 const { google } = require('googleapis');
 const fs = require('fs').promises;
 const path = require('path');
+const { authorize, listLabels } = require('./authenticate');
 
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
@@ -17,60 +18,23 @@ const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 app.use(express.json());
 app.use(cors());
 
-async function loadSavedCredentialsIfExist() {
+app.get('/authorize', async (req, res) => {
   try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
-}
+    const auth = await authorize();
 
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
-}
+    const gmail = google.gmail({ version: 'v1', auth });
+    const response = await gmail.users.labels.list({
+      userId: 'me',
+    });
 
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
-}
+    const labels = response.data.labels;
 
-async function listLabels(auth) {
-  const gmail = google.gmail({ version: 'v1', auth });
-  const res = await gmail.users.labels.list({
-    userId: 'me',
-  });
-  const labels = res.data.labels;
-  if (!labels || labels.length === 0) {
-    console.log('No labels found.');
-    return;
+    // Send the labels as JSON in the response
+    res.json({ labels });
+  } catch (error) {
+    console.error('Error', error.message);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
   }
-  console.log('Labels:');
-  labels.forEach((label) => {
-    console.log(`- ${label.name}`);
-  });
-}
-
-authorize().then(listLabels).catch(console.error);
+});
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const URL = 'http://localhost:5432';
 
 function App() {
-  const [userText, setUserText] = useState('');
+  const [list, setList] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,12 +17,30 @@ function App() {
     }
   };
 
+  const handleEmailButton = async () => {
+    console.log('click');
+    try {
+      const response = await axios.get(`${URL}/authorize`);
+      console.log(response.data.labels);
+      setList([...response.data.labels]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkList = () => {
+    console.log(list);
+  };
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <input placeholder="email" />
         <button type="submit">Submit</button>
       </form>
+
+      <button onClick={handleEmailButton}>email</button>
+      <div>{list.length && list.map((li) => <div>{li.id}</div>)}</div>
     </div>
   );
 }
