b9f05a487d0e7301dcc51af97d30e4bcb3ec0ce8
Josh Genereux <jgenereux@stratacache.com>
2024-01-18 14:22:25 -0500

    tossing the openai api, trying aws now

--- server/controllers/readEmailController.js
+++ server/controllers/readEmailController.js
@@ -1,16 +1,7 @@
-const { OpenAI } = require('openai');
-
 const key = process.env.OPENAI_API_KEY;
-const openai = new OpenAI({ apiKey: key });
 
 const readEmailController = async (req, res) => {
   try {
-    const message = req.body.message;
-    const response = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: 'This is a test' }],
-      model: 'gpt-3.5-turbo',
-    });
-    console.log(response.data);
   } catch (error) {
     console.log(error);
     res.status(400).send({
