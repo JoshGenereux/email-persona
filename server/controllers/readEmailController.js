714a2393b63b5029bbd4bb9c7a440cb22a62e57e
Josh Genereux <jgenereux@stratacache.com>
2024-01-19 15:39:12 -0500

    got hooked up to aws comprehend api, now need to figure out where to go from here

--- server/controllers/readEmailController.js
+++ server/controllers/readEmailController.js
@@ -1,7 +1,28 @@
-const key = process.env.OPENAI_API_KEY;
+const AWS = require('aws-sdk');
+const key = process.env.AWS_ACCESS_KEY;
+const secretKey = process.env.SECRET_ACCESS_KEY;
+
+AWS.config.update({
+  accessKeyId: key,
+  secretAccessKey: secretKey,
+  region: 'us-east-1',
+});
+const comprehend = new AWS.Comprehend();
 
 const readEmailController = async (req, res) => {
   try {
+    const params = {
+      LanguageCode: 'en',
+      Text: req.body.message,
+    };
+
+    comprehend.detectSentiment(params, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Sentiment', data);
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(400).send({
